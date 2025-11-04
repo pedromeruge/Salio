b2544c464efa0ddcb795cfbcf36659fffe1e1237
Aecesaje <martimmaci02@gmail.com>
2025-11-04 15:38:06 +0000

    level select

--- Assets/Scripts/MainMenu/LevelSelect.cs
+++ Assets/Scripts/MainMenu/LevelSelect.cs
@@ -11,7 +11,22 @@ public class LevelSelect : MonoBehaviour
 
     public void TransitionToLevelEasy()
     {
+        SceneManager.LoadSceneAsync("single_maze_easy");
+    }
+
+    public void TransitionToLevelMedium()
+    {
+        SceneManager.LoadSceneAsync("single_maze_medium");
+    }
+
+    public void TransitionToLevelHard()
+    {
         SceneManager.LoadSceneAsync("single_maze");
+    }
+
+    public void TransitionToLevelExtra()
+    {
+        SceneManager.LoadSceneAsync("two_mazes");
     }
 
 }
